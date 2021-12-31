3369931460904e07a235775c9c84e0e7f099cd04
GitPit <rifflifter@gmail.com>
2021-12-31 13:12:12 -0500

    added email, city, country, state to Table user

--- schema.sql
+++ schema.sql
@@ -6,6 +6,10 @@ CREATE TABLE user(
     id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30) DEFAULT NOT NULL,
     last_name VARCHAR(30) DEFAULT NOT NULL,
+    email VARCHAR(100) DEFAULT NOT NULL,
+    country VARCHAR(30),
+    city VARCHAR(30),
+    state_prov VARCHAR(30),
     PRIMARY KEY (id)
 );
 
