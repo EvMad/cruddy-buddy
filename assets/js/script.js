9ad4b829cc8b3dc99efa17cbd38e0f08d1ee9eff
GitPit <rifflifter@gmail.com>
2022-01-03 21:31:10 -0500

    eventListener / if/else submit / setup for mvc

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,4 +9,15 @@ const formSubmit = async (event) => {
 
     const name = document.querySelector("#firstName").value.trim();
     const email = document.querySelector("#email").value.trim();
-}
+
+    if (name && email) {
+        console.log("first name and email submitted");
+    }
+    else
+    {
+        alert("Failed to enter name and email");
+    };
+
+    document
+    .addEventListener('submit', formSubmit);
+};
